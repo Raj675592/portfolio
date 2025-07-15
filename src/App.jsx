f4d43de4d852c773d2db50501a446876fc68ddb5
Raj675592 <190810366+Raj675592@users.noreply.github.com>
2025-07-15 08:58:25 +0530

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-
+import './App.css';
 import Footer from './components/Footer';
 import RollDice from './components/RollDice';
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => (
       <img src="profile.png" alt="Profile" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }} />
     </div>
     <h1>Rajkumar Ahirwar</h1>
-    <h2>Software Developer (Aspiring)</h2>
+    {/* <h2>Software Developer (Aspiring)</h2> */}
     <p>Welcome to my personal portfolio website! I love building things with code and am currently studying at IIT Kanpur.</p>
   </section>
 );
